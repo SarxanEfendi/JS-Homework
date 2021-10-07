380cdddfaf0fe20af8e311100a76950c6d3bcee0
sarxanefendi <sarkhanaa@code.edu.az>
2021-10-07 16:20:28 +0400

    .....

--- homework/js/main.js
+++ homework/js/main.js
@@ -1,35 +1,35 @@
 // array
-var arr =[1,2,3,4,5,6,7,8,9]
-var resArr = []
+var arr =[1,2,3,4,5,6,7,8,9];
+var resArr = [];
 function countTwo (arr ,resArr) {
     for (var i = 0; i < arr.length; i++) {
         if(arr[i]%2 === 0){
-            resArr.push(arr[i])
+            resArr.push(arr[i]);
         }
         
     }
-    return resArr
+    return resArr;
 }
-console.log(countTwo(arr,resArr))
+console.log(countTwo(arr,resArr));
 
 // calculator
 
 function calculator(firstElement,seccontElement,sign){
     if(sign === '+'){
-        return firstElement+seccontElement
+        return firstElement+seccontElement;
     }
     else if (sign === '-'){
-        return firstElement-seccontElement
+        return firstElement-seccontElement;
     }
     else if(sign === '*'){
-        return firstElement*seccontElement
+        return firstElement*seccontElement;
     }
     else if (sign === '/'){
-        return firstElement/seccontElement
+        return firstElement/seccontElement;
     }
     else{
-        return undefined
+        return undefined;
     }
 }
 
-console.log(calculator(5,6,'*'))
+console.log(calculator(5,6,'+'));
